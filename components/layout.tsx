42b0818cf2333529f612576aeccf74dea22f9ec1
SyedUmair282 <umairsyed282@gmail.com>
2023-08-13 17:54:05 +0500

    children update

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Footer from './footer';
 import Header from './header';
 
-const Layout:React.FC = ({children}) => {
+const Layout:React.FC = ({children}:any) => {
   return (
     <div className="flex flex-col h-screen bg-gradient-to-r to-slate-900 from-gray-600">
         <Header/>
